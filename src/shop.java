0dc46b689023b972aa6fd764557e77ea53ebfc0d
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-05-06 09:27:59 +0700

    Initial commit

---
+++ src/shop.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class shop {
+    public static void main(String[] args) {
+        ProgrammingBook[] programmingBooks = new ProgrammingBook[5];
+        programmingBooks[0] = new ProgrammingBook("BK1", "Think Java", 200.000, "Allen B.Downey & Chris Mayfield", "Java", "GridWorld");
+        programmingBooks[1] = new ProgrammingBook("BK2", "Think in Java (4th Edition)", 100.000, "Bruce Eckel", "Java", "JSF");
+        programmingBooks[2] = new ProgrammingBook("BK3", "Effective Java", 300.000, "Joshua Bloch", "Java", "Struts");
+        programmingBooks[3] = new ProgrammingBook("BK4", "Giáo trình Kỹ thuật lập trình C căn bản & nâng cao ", 230.000, "Phạm Văn Ất", "C", "Hibernate");
+        programmingBooks[4] = new ProgrammingBook("BK5", "C# Yellow Book", 570.000, "Rob Miles", "C#", "GWT");
+
+        FictionBook[] fictionBooks = new FictionBook[5];
+        fictionBooks[0] = new FictionBook("FB1", "Harry Porter" , 650.000, "J.K. Rowling", "Fantasy" );
+        fictionBooks[1] = new FictionBook("FB2", "Frankenstein" , 450.000, "Mary Shelley", "Horror" );
+        fictionBooks[2] = new FictionBook("FB3", "Charlotte's Web" , 250.000, "E.B. White", "For Kid" );
+        fictionBooks[3] = new FictionBook("FB4", "The Book Thief" , 400.000, "Robert Crayola", "War" );
+        fictionBooks[4] = new FictionBook("FB5", "Dracula" , 300.000, "Bram Stoker", "Horror" );
+
+        double totalPrice = 0;
+        int pbCount = 0;
+
+        for (ProgrammingBook pb : programmingBooks) {
+            totalPrice += pb.getPrice();
+            if (pb.getLanguage().equals("Java"))
+                pbCount++;
+        }
+        for (FictionBook fb : fictionBooks) {
+            totalPrice += fb.getPrice();
+
+        }
+        System.out.println("Total price of ten books is " + totalPrice + " VND");
+        System.out.println("There are " + pbCount + " books about Java");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What book you want to find?");
+        String findingBook = sc.nextLine();
+
+
+        for (ProgrammingBook pb : programmingBooks) {
+            if (pb.getName().equals(findingBook))
+                System.out.println("Price of finding book is : " + pb.getPrice() + " VND");
+        }
+        for (FictionBook fb : fictionBooks) {
+            if (fb.getName().equals(findingBook))
+                System.out.println("Price of finding book is : " + fb.getPrice() + " VND");
+        }
+    }
+}
